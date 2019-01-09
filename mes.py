656b5680546444e3bcfcfa107f0f89e0eb5c37c7
Max <41266954+JiangDayuan@users.noreply.github.com>
2019-01-09 17:41:16 +0800

    Add list in mes.py

--- mes.py
+++ mes.py
@@ -1,8 +1,7 @@
 import codecs
 import os
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
+import tkinter
+from tkinter import ttk
 
 class piwebconfig():
     def __init__(self, file_dir, pi_txt):
@@ -37,6 +36,17 @@ def msel_change():
     msel_gen = piwebconfig(msel_para_dir, 'criteria.para')
     msel_gen.msel()
 
-
+def go(*args):   #处理事件，*args表示可变参数  
+        print(comboxlist.get()) #打印选中的值 
+        
 if __name__ == "__main__":
-    msel_change()
+    #msel_change()
+    win=tkinter.Tk() #构造窗体  
+    comvalue=tkinter.StringVar()#窗体自带的文本，新建一个值  
+    comboxlist=ttk.Combobox(win,textvariable=comvalue) #初始化  
+    comboxlist["values"]=("1","2","3","4")  
+    comboxlist.current(0)  #选择第一个  
+    comboxlist.bind("<<ComboboxSelected>>",go)  #绑定事件,(下拉列表框被选中时，绑定go()函数)  
+    comboxlist.pack()  
+      
+    win.mainloop() #进入消息循环 
