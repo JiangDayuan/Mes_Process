9bae907ebc533a0405bf2bac9eb3566d5ff5114a
Max Jiang <maxjiangfy@126.com>
2019-01-15 22:41:30 +0800

    modify mes_pros.py
    add program_list in json
    change json generate path
    add error deal in assignment function

--- mes_pros.py
+++ mes_pros.py
@@ -6,7 +6,7 @@ import sys
 
 import shutil
 
-#import tkinter
+import tkinter
 #from tkinter import ttk
 
 class system_config():
@@ -37,7 +37,7 @@ class system_config():
         self.ai = json_f['autoimporter']
         self.data = json_f['data']
         self.server = json_f['server']
-        
+
 class netaccess(system_config):
     def __init__(self):
         system_config.__init__(self)
@@ -50,27 +50,28 @@ class netaccess(system_config):
         self.data_dir = self.data['parent'][1]
         self.data_user = self.data['parent'][2]
         self.data_pwd = self.data['parent'][3]
-    
+
     def autoimporter_access(self):
-        warn = None
         ping_ai = 'Ping.exe -n 1 '+self.ai_ping
         status = os.system(ping_ai)
         if status != 0:
-            warn = "无法连接到Auto Importer服务器({})!\n请检查网络连接后再试".format(self.ai_ping)
-            return warn
+            warn = False
         else:
             access = 'net use '+self.ai_dir+' /user:'+self.ai_user+' '+self.ai_pwd
             os.system(access)
-            
+            warn = True
+        return warn
+
     def fileserver_access(self):
         ping_server = 'Ping.exe -n 1 '+self.data_ping
         status = os.system(ping_server)
         if status != 0:
-            self.warn = "无法连接到文件服务器({})!\n请检查网络连接后再试".format(self.data_ping)
+            warn = False
         else:
             access = 'net use '+self.data_dir+' /user:'+self.data_user+' '+self.data_pwd
             os.system(access)
-
+            warn = True
+        return warn
 
 class piwebconfig(system_config):
     def __init__(self, pi_txt):
@@ -83,7 +84,7 @@ class piwebconfig(system_config):
         system_config.__init__(self)
         self.txt = os.path.join(self.para_dir, pi_txt)
         self.piweb_txt_parser()
-    
+
     def piweb_txt_parser(self):
         """
         将创建实例时输入的txt或para文件中的信息转换成字典，并输出
@@ -99,7 +100,7 @@ class piwebconfig(system_config):
                     txt_data[key] = value
         os.remove(self.txt)
         self.txt_data = txt_data
-    
+
     def msel(self, example='example.msel', search='search.msel'):
         """
         将模板msel文件中的序列号值修改为criteria.para中包含的序列号
@@ -126,11 +127,11 @@ class piwebconfig(system_config):
     def csv_generate(self, csv_data=None, ctl_info=None, max_attribute=30):
         """
         创建csv文件，准备上传到PiWeb Auto Importer中
-        1.输入: 
+        1.输入:
             (1)csv_data:字典，包含需要生成csv中测量特性的内容，
                         默认None，如果为None则使用创建实例时输入的txt或para文件生成的字典
             (2)ctl_info:字典，包含需要创建探针的信息
-        2.输出: 
+        2.输出:
             输出一个保存在Local_Temp中的csv文件
         """
         #csv文件的保存路径(config.json中的temp路径)
@@ -139,34 +140,41 @@ class piwebconfig(system_config):
         if csv_data==None:
             csv_data = self.txt_data
         #如果文件中存在‘Serial_Number’,则使用序列号作为csv的文件名
-        if 'Serial_Number' in csv_data.keys():
-            csv_name = csv_data['Serial_Number']+'.csv'
-            self.config_path_reader()
-            target_dir = self.data['temp'] #csv文件的保存路径(config.json中的temp路径)
-            if not os.path.exists(target_dir):
-                os.makedirs(target_dir)
-            self.csv_dir = os.path.join(target_dir, csv_name)
-            #为了获取统一配置，设置足够多的Measurement Attribute,不足的数量填入空值
-            empty_number = max_attribute - len(csv_data)
-            #将txt或para文件的内容转换成csv的格式
-            with open(self.csv_dir, 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                for key, value in csv_data.items():
-                    writer.writerow([key,value])
-                #如果Measurement Attribute数量不足，则自动补充
-                while empty_number > 0:
-                    writer.writerow(['Empty', 'None'])
-                    empty_number -= 1
-                #添加Measurement Value Attribute
-                writer.writerow(['Mes_Info', 0])
-                #添加储存探针信息的Measurement Value Attribute
-                if ctl_info != None:
-                    for key, value in ctl_info.items():
-                        if key != "fixture":
-                            writer.writerow(["Probe_Info", key, value[2]])
-        else:
-            process_status = "模板中输出的信息缺失，请联系管理员或重新安装模板"
-        return process_status
+        csv_name = csv_data['Serial_Number']+'.csv'
+        self.config_path_reader()
+        target_dir = self.data['temp'] #csv文件的保存路径(config.json中的temp路径)
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+        self.csv_dir = os.path.join(target_dir, csv_name)
+        #为了获取统一配置，设置足够多的Measurement Attribute,不足的数量填入空值
+        empty_number = max_attribute - len(csv_data)
+        #将txt或para文件的内容转换成csv的格式
+        with open(self.csv_dir, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for key, value in csv_data.items():
+                writer.writerow([key,value])
+            #如果Measurement Attribute数量不足，则自动补充
+            while empty_number > 0:
+                writer.writerow(['Empty', 'None'])
+                empty_number -= 1
+            #添加Measurement Value Attribute
+            writer.writerow(['Mes_Info', 0])
+            #添加储存探针信息的Measurement Value Attribute
+            if ctl_info != None:
+                for key, value in ctl_info.items():
+                    if key != "fixture":
+                        writer.writerow(["Probe_Info", key, value[2]])
+
+class UI(tkinter.Tk):
+    def __init__(self):
+        super().__init__()
+
+
+        
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 def msel_change():
     msel_gen = piwebconfig('criteria.para')
@@ -177,16 +185,30 @@ def assignment_data():
         assign = piwebconfig('assignment.para')
         assign.csv_generate()
         net = netaccess()
-        net.autoimporter_access()
-        if not os.path.exists(assign.ai['userlist']):
-                os.makedirs(assign.ai['userlist'])
-        if not os.path.exists(assign.ai['server']):
-                os.makedirs(assign.ai['server'])
-        shutil.copy(assign.csv_dir, assign.ai['userlist'])
-        shutil.move(assign.csv_dir, assign.ai['server'])
+        if True:#net.autoimporter_access():
+            mkdir(assign.ai['userlist'])
+            mkdir(assign.ai['server'])
+            shutil.copy(assign.csv_dir, assign.ai['userlist'])
+            shutil.copy(assign.csv_dir, assign.ai['server'])
+        else:
+            mkdir(assign.data['failure'])
+            shutil.copy(assign.csv_dir, os.path.join(assign.data['failure']))
     except FileNotFoundError:
         print("没有发现文件！")
-    
+    except shutil.Error as e:
+        print('有文件\n{}'.format(e))
+    except KeyError:
+        print("文件有问题！")
+    else:
+        os.remove(assign.csv_dir)
+    finally:
+        temp_dir = system_config()
+        file_list = os.listdir(temp_dir.para_dir)
+        if len(file_list) != 0:
+            for file in file_list:
+                os.remove(os.path.join(temp_dir.para_dir, file))
+
+
 def json_config():
     a = {
         'autoimporter':
@@ -205,13 +227,14 @@ def json_config():
         'server':
         {
             'userlist': r'C:\Server\User_List.dfm',
+            'programlist': r'C:\Server\Program_List.dfm',
             'main': r'http://10.202.0.200:8088'
         }
         }
-    with open(r'C:\CC_Process_System\Contract_Measurement_Process\Program_Files\q-Flow\relative_files\config.json', 'w') as j:
+    with open(r'relative_files\config.json', 'w') as j:
         json.dump(a, j, sort_keys=False, indent=2)
 
 if __name__ == "__main__":
     assignment_data()
+    shutil.copyfile('assignment.para', r'temp\assignment.para')
     pass
-    
